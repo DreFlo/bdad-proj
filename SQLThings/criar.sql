2540069a9d0c4f3cca3ac0ecdce7395012b3538c
André Flores <andrejesusferflores@gmail.com>
2021-03-23 15:55:53 +0000

    idk

--- SQLThings/criar.sql
+++ SQLThings/criar.sql
@@ -1,127 +1,125 @@
-/* using number for dates */
 /* derived atrributes not being calculated */
 /* all FK delete and update actions set to cascade */
-
 
 pragma foreign_keys = ON;
 
 drop table if exists Country;
 
 create table Country (
-	locID number constraint Country_PK primary key not null,
+	locID integer constraint Country_PK primary key not null,
 	name text constraint Country_name_not_null not null,
-	caseNumber number,
-	noVaccinated number,
-	population number
+	caseNumber integer,
+	noVaccinated integer,
+	population integer
 );
 
 drop table if exists District;
 
 create table District (
-	locID number constraint District_PK primary key not null,
+	locID integer constraint District_PK primary key not null,
 	name text constraint District_name_not_null not null,
-	caseNumber number,
-	noVaccinated number,
-	population number,
-	countryID number constraint District_FK references Country(locID) on delete cascade on update cascade not null
+	caseNumber integer,
+	noVaccinated integer,
+	population integer,
+	countryID integer constraint District_FK references Country(locID) on delete cascade on update cascade not null
 );
 
 drop table if exists County;
 
 create table County (
-	locID number constraint Country_PK primary key not null,
+	locID integer constraint Country_PK primary key not null,
 	name text constraint County_name_not_null not null,
-	caseNumber number,
-	noVaccinated number,
-	population number,
-	districtID number constraint County_FK references District(locID) on delete cascade on update cascade not null 
+	caseNumber integer,
+	noVaccinated integer,
+	population integer,
+	districtID integer constraint County_FK references District(locID) on delete cascade on update cascade not null 
 );
 
 drop table if exists Parish;
 
 create table Parish (
-	locID number constraint Parish_PK primary key not null,
+	locID integer constraint Parish_PK primary key not null,
 	name text constraint Parish_name_not_null not null,
-	caseNumber number,
-	noVaccinated number,
-	population number,
-	countyID number constraint Parish_FK references County(locID) on delete cascade on update cascade not null 
+	caseNumber integer,
+	noVaccinated integer,
+	population integer,
+	countyID integer constraint Parish_FK references County(locID) on delete cascade on update cascade not null 
 );
 
 drop table if exists Vaccine;
 
 create table Vaccine (
-	vacID number constraint Vaccine_PK primary key not null,
-	name number constraint Vaccine_name_not_null not null,
-	doseNumber number constraint Vaccine_doses_greater_than_one check(doseNumber >= 1) not null
+	vacID integer constraint Vaccine_PK primary key not null,
+	name integer constraint Vaccine_name_not_null not null,
+	doseNumber integer constraint Vaccine_doses_greater_than_one check(doseNumber >= 1) not null
 );
 
 drop table if exists VaccinationAmount;
 
 create table VaccinationAmount (
-	parishID number constraint VaccinationAmount_Parish_FK references Parish(locID) on delete cascade on update cascade not null,
-	vacID number constraint VaccinationAmount_Vaccine_FK references Vaccine(vacID) on delete cascade on update cascade not null,
-	vaccination number constraint VaccinationAmount_range check(vaccination >= 0),
+	parishID integer constraint VaccinationAmount_Parish_FK references Parish(locID) on delete restrict on update cascade not null,
+	vacID integer constraint VaccinationAmount_Vaccine_FK references Vaccine(vacID) on delete restrict on update cascade not null,
+	vaccination integer constraint VaccinationAmount_range check(vaccination >= 0),
 	constraint VaccinationAmount_PK primary key(parishID, vacID)
 );
 
 drop table if exists Manufacturer;
 
 create table Manufacturer (
-	manID number constraint Manufacturer_PK primary key not null,
+	manID integer constraint Manufacturer_PK primary key not null,
 	name text constraint Manufacturer_name_not_null not null
 );
 
 drop table if exists VaccineManufacturer;
 
 create table VaccineManufacturer (
-	vacID number constraint VaccineManufacturer_Vaccine_FK references Vaccine(vacID) on delete cascade on update cascade not null,
-	manID number constraint VaccineManufacturer_Manufacturer_FK references Manufacturer(manID) on delete cascade on update cascade not null,
+	vacID integer constraint VaccineManufacturer_Vaccine_FK references Vaccine(vacID) on delete restrict on update cascade not null,
+	manID integer constraint VaccineManufacturer_Manufacturer_FK references Manufacturer(manID) on delete restrict on update cascade not null,
 	constraint VaccineManufacturer_PK primary key(vacID, manID)
 );
 
 drop table if exists Strain;
 
 create table Strain (
-	strainID number constraint Strain_PK primary key not null,
+	strainID integer constraint Strain_PK primary key not null,
 	designation text constraint Strain_name_not_null not null,
-	countryID number constraint Strain_FK references Country(locID) on delete cascade on update cascade not null
+	countryID integer constraint Strain_FK references Country(locID) on delete restrict on update cascade not null
 );
 
 drop table if exists NursingHome;
 
 create table NursingHome (
-	nursingHomeID number constraint NursingHome_PK primary key not null,
+	nursingHomeID integer constraint NursingHome_PK primary key not null,
 	name text
 );
 
 drop table if exists EmploymentSector;
 
 create table EmploymentSector (
-	sectorID number constraint EmploymentSector_PK primary key not null,
+	sectorID integer constraint EmploymentSector_PK primary key not null,
 	name text,
-	contactFrequency number,
+	contactFrequency integer,
 	constraint contactFrequencyRange check((contactFrequency >= 0 and contactFrequency <= 5) or contactFrequency == null)
 );
 
 drop table if exists Ethnicity;
 
 create table Ethnicity (
-	etniID number constraint Ethnicity_PK primary key not null,
+	etniID integer constraint Ethnicity_PK primary key not null,
 	name text constraint Ethnicity_name_not_null not null
 );
 
 drop table if exists COVIDCase;
 
 create table COVIDCase (
-	caseID number constraint COVIDCase_PK primary key not null,
-	detectionDate number,
-    endDate number,
-	birthYear number,
-	outcome number,
-	parishID number constraint COVIDCase_Parish_FK references Parish(locID) on delete cascade on update cascade not null,
-	strainID number constraint COVIDCase_Strain_FK references Strain(strainID) on delete cascade on update cascade not null,
-	nursingHomeID number constraint COVIDCase_NursingHome_FK references NursingHome(nursingHomeID) on delete cascade on update cascade,
+	caseID integer constraint COVIDCase_PK primary key not null,
+	detectionDate integer,
+    endDate integer,
+	birthYear integer,
+	outcome integer,
+	parishID integer constraint COVIDCase_Parish_FK references Parish(locID) on delete restrict on update cascade not null,
+	strainID integer constraint COVIDCase_Strain_FK references Strain(strainID) on delete restrict on update cascade not null,
+	nursingHomeID integer constraint COVIDCase_NursingHome_FK references NursingHome(nursingHomeID) on delete restrict on update cascade,
 	constraint date_range check(detectionDate <= endDate),
 	constraint outcome_range check(outcome >= 0 and outcome <= 2)
 );
@@ -129,15 +127,15 @@ create table COVIDCase (
 drop table if exists EthnicityCOVIDCase;
 
 create table EthnicityCOVIDCase (
-	caseID number constraint EthnicityCOVIDCase_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
-	etniID number constraint EthnicityCOVIDCase_Ethnicity_FK references Ethnicity(etniID) on delete cascade on update cascade not null,
+	caseID integer constraint EthnicityCOVIDCase_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
+	etniID integer constraint EthnicityCOVIDCase_Ethnicity_FK references Ethnicity(etniID) on delete restrict on update cascade not null,
 	constraint EthnicityCOVIDCase_PK primary key(caseID, etniID)
 );
 
 drop table if exists Hospital;
 
 create table Hospital (
-	hospitalID number constraint Hospital_PK primary key not null,
+	hospitalID integer constraint Hospital_PK primary key not null,
 	name text,
 	parishID constraint Hospital_FK references Parish(locID) on delete cascade on update cascade not null
 );
@@ -145,39 +143,39 @@ create table Hospital (
 drop table if exists Hospitalization;
 
 create table Hospitalization (
-	hospStayID number constraint Hospitalization_PK primary key not null,
-	startDate number,
-	endDate number,
-	hospitalID number constraint Hospitalization_Hospital_FK references Hospital(hospitalID) on delete cascade on update cascade not null,
-	caseID number constraint Hospitalization_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
+	hospStayID integer constraint Hospitalization_PK primary key not null,
+	startDate integer,
+	endDate integer,
+	hospitalID integer constraint Hospitalization_Hospital_FK references Hospital(hospitalID) on delete restrict on update cascade not null,
+	caseID integer constraint Hospitalization_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
 	constraint date_range check(startDate <= endDate)
 );
 
 drop table if exists ICUStay;
 
 create table ICUStay (
-	ICUStayID number constraint ICUStay_PK primary key not null,
-	startDate number,
-	endDate number,
-	hospStayID number constraint ICUStay_FK references Hospitalization(hospStayID) on delete cascade on update cascade not null,
+	ICUStayID integer constraint ICUStay_PK primary key not null,
+	startDate integer,
+	endDate integer,
+	hospStayID integer constraint ICUStay_FK references Hospitalization(hospStayID) on delete cascade on update cascade not null,
 	constraint date_range check(startDate <= endDate) 
 );
 
 drop table if exists Ventilation;
 
 create table Ventilation (
-	ventID number constraint Ventilation_PK primary key not null,
-	startDate number,
-	endDate number,
-	ICUStayID number constraint Ventilation_FK references ICUStay(ICUStayID) on delete cascade on update cascade not null,
+	ventID integer constraint Ventilation_PK primary key not null,
+	startDate integer,
+	endDate integer,
+	ICUStayID integer constraint Ventilation_FK references ICUStay(ICUStayID) on delete cascade on update cascade not null,
 	constraint date_range check(startDate >= endDate)
 );
 
 drop table if exists EmployedIn;
 
 create table EmployedIn (
-	caseID number constraint EmployedIn_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
-	sectorID number constraint EmployedIN_EmploymentSector_FK references EmploymentSector(sectorID) on delete cascade on update cascade not null,
+	caseID integer constraint EmployedIn_COVIDCase_FK references COVIDCase(caseID) on delete cascade on update cascade not null,
+	sectorID integer constraint EmployedIN_EmploymentSector_FK references EmploymentSector(sectorID) on delete restrict on update cascade not null,
 	constraint EmployedIn_PK primary key(caseID, sectorID)
 );
 
---
+++ SQLThings/povoar.sql
@@ -0,0 +1,32 @@
+/* Countries */
+insert into Country values(0, 'MiddleEarth', 0, 0, 0);
+insert into Country values(1, 'Westeros', 0, 0, 0);
+
+/* District */
+insert into District values(0, 'The Shire', 0, 0, 0, 0);
+insert into District values(1, 'Númenor', 0, 0, 0, 0);
+insert into District values(2, 'Rohan', 0, 0, 0, 0);
+
+/* Counties */
+insert into County values(0, 'Bag End', 0, 0, 0, 0);
+
+/* Parishes */
+insert into Parish values(0, 'Bag End', 0, 0, 0, 0);
+
+/* Manufacturers */
+insert into Manufacturer values(0, 'Imuuuunizantes');
+
+/* Vaccines */
+insert into Vaccine values(0, 'GoneCOVID', 1);
+
+/* VaccineManufacturers */
+insert into VaccineManufacturer values(0, 0);
+
+/* VaccinationAmounts */
+insert into VaccinationAmount values(0, 0, 300);
+
+/* Strains */
+insert into Strain values(0, 'I.D.K.0.0.0', 1);
+
+/* Hospitals */
+insert into Hospital values(0, 'Bilbos Backyard', 0);
